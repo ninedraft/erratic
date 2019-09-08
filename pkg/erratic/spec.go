02998de05734f1f5ff169bed0a5b9bfe3a168594
ninedraft <clamores.pro@gmail.com>
2019-09-08 23:59:47 +0300

    add basic ast generation to error spec

--- pkg/erratic/spec.go
+++ pkg/erratic/spec.go
@@ -1,21 +1,35 @@
 package erratic
 
+import (
+	"go/ast"
+
+	"github.com/ninedraft/erratic/pkg/gen"
+)
+
+var pkgTemplate = &gen.Import{
+	Name: "template",
+	Path: "text/template",
+}
+
 type Package struct {
 	Name   string  `json:"package" yaml:"package" toml:"package"`
 	Errors []Error `json:"errors" toml:"error"`
 }
 
-type Error struct {
-	Name     string  `json:"name"`
-	Code     int     `json:"code"`
-	Message  string  `json:"message"`
-	UnwrapOn string  `json:"unwrap_on"`
-	Fields   []Field `json:"fields" toml:"field"`
+func (pkg *Package) AST() *ast.File {
+	var decls = make([]ast.Decl, 0, len(new(Error).Declarations())*len(pkg.Errors))
+	for _, err := range pkg.Errors {
+		decls = append(decls, err.Declarations()...)
+	}
+	return &ast.File{
+		Name:    gen.Ident(pkg.Name),
+		Imports: pkg.Imports(),
+		Decls:   decls,
+	}
 }
 
-type Field struct {
-	Name    string            `json:"name"`
-	Type    string            `json:"type"`
-	Tags    map[string]string `json:"tags"`
-	Default interface{}       `json:"default"`
+func (pkg *Package) Imports() []*ast.ImportSpec {
+	return []*ast.ImportSpec{
+		pkgTemplate.AST(),
+	}
 }
